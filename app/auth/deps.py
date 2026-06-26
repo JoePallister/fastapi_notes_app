d96c5b0b0dc797eadeb3072338b49d7b2e55850a
JoePallister <joepallister@gmail.com>
2026-06-26 20:02:06 +0100

    get_user_now_decodes_jwt

--- app/auth/deps.py
+++ app/auth/deps.py
@@ -3,6 +3,15 @@ from fastapi import HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi import Depends
 
+from app.models.user import User
+from app.db.deps import get_db
+from sqlalchemy.orm import Session
+from jose import JWTError, jwt
+
+# Duplication here, should be refactored to avoid hardcoding the secret key and algorithm in multiple places.
+SECRET_KEY = "dev-secret"
+ALGORITHM = "HS256"
+
 bearer_scheme = HTTPBearer()
 
 users = []
@@ -11,11 +20,20 @@ Credentials = Annotated[HTTPAuthorizationCredentials, Depends(bearer_scheme)]
 
 
 def get_current_user(
-    credentials: Credentials,
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+    db: Session = Depends(get_db),
 ):
-    token = credentials.credentials  # already stripped of "Bearer "
+    token = credentials.credentials
 
-    if token != "user1":
-        raise HTTPException(401)
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = int(payload["sub"])
+    except JWTError:
+        raise HTTPException(401, "Invalid token")
 
-    return token
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(401, "User not found")
+
+    return user
