68a04cd2f8e08ca314907790ffdd3bc789918466
JoePallister <joepallister@gmail.com>
2026-06-26 14:39:22 +0100

    basic html frontend

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.api import notes
 
 app = FastAPI()
 
 app.include_router(notes.router, prefix="/notes", tags=["notes"])
+app.mount("/", StaticFiles(directory="app/static", html=True), name="static")
